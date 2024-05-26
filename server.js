9bec7a1b0463344b1cec19eda5a9736394b377c4
aiXpertLab <aiXpertLab@gmail.com>
2024-05-26 19:22:20 -0400

    v1

--- server.js
+++ server.js
@@ -1,18 +1,36 @@
+//0. System   https://segmentfault.com/a/1190000022039572
 const express = require('express');
-const app = express(); // Creates an Express application
+const cors = require('cors');
 
-// Middleware (optional)
-app.use(express.json()); // Parse incoming JSON requests
+// 1. app
+const app = express();
+app.use(cors())
+app.use(express.static('public'));  //static
 
-// Routes
-app.get('/', (req, res) => {res.send('Hello from my Express server!');});
+// 2. routes
+const mainRouter = require('./routes/main');
+const apiRouter = require('./routes/api');
+app.use('/', mainRouter)
+app.use('/api', apiRouter)
 
-// Error handling (optional)
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
-});
+// 3. middlewares
+const errorHandler = require('./middlewares/error-handler');
+app.use((req, res, next) => {
+    console.log(`请求日志：${req.method} ${req.url} ${new Date()}`);
+    next(); // 放行
+}) //可以发现：任何请求进来都会先打印请求日志，然后才会执行具体的业务处理函数
 
-// Start the server
-const port = process.env.PORT || 3000; // Use environment variable or default port
-app.listen(port, () => {console.log(`Server listening on port ${port}`);});
+// 4. template engine
+app.set('views', './hbs');    // 指定模板存放目录
+app.set('view engine', 'hbs');  // 指定模板引擎为 Handlebars
+
+//5. body-parser config;
+const bodyParser = require('body-parser');
+const path = require('path');
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true }));
+app.use(bodyParser.json());
+
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => {console.log(`Server listening on port ${PORT}`);});
